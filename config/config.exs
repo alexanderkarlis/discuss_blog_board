6fbfda7a8ff82150af184cbfc8aa95be23f61cc6
alexanderkarlis <alexander.karlis@reactivecore.com>
2019-12-03 22:48:11 -0500

    building the app steadily

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [default_scope: "user,public_repo"] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "12f288664a1c490e195c",
+  client_secret: System.get_env("github_client_secret")
---
+++ web/controllers/auth_controller.ex
@@ -0,0 +1,17 @@
+defmodule Discuss.AuthController do
+    use Discuss.Web, :controller
+    plug Ueberauth
+
+    alias Discuss.User
+
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
+        user_params = %{
+            token: auth.credentials.token,
+            email: auth.info.email,
+            provider: provider
+        }
+
+        changeset = User.changeset{%User{}, user_params}
+        IO.inspect(user_params)
+    end
+end
